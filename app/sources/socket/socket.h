218013889bbd2e70876358ace8d7e046a2ff5c3c
Bafometz <pro100swt@gmail.com>
2024-01-19 22:39:53 +0300

    Добавил доку и немного подправил класс сокета

--- app/sources/socket/socket.h
+++ app/sources/socket/socket.h
@@ -6,25 +6,45 @@
 
 #include "../data_package/datatpackage.h"
 #include "../io_device/iodevice.h"
-#include <atomic>
 #include <memory>
 
+/**
+ * @brief Класс сокета, реализовывает в себе не все типы сокетов, только локальный и сетевой сокерт и только tcp
+ */
 class Socket : public IODevice
 {
+    /**
+     * @brief Тип сокета
+     */
     enum class SocketType
     {
-        LOCAL,
-        ETHERNET,
+        LOCAL,     ///< Локлальный
+        ETHERNET,  ///< Сетевой
     };
 
   public:
+    /**
+     * @brief Создаёт мастер сокет для подключений, остальные сокеты будут пораждаться от мастер сокета
+     * @param Адресс сервера
+     * @param Порт к которому подключаться
+     * @param Тип сокета
+     * @param Блокирующий или неблокирующий сокет
+     */
+    explicit Socket(const std::string &address, int portNum, SocketType = SocketType::ETHERNET, bool nonBlockingMode = false) noexcept;
+
+    /**
+     * @brief Конструктор для пораждённых сокетов, нужно после подключения, т.к. даёт возможность читать/писать + реализует RAII
+     */
     explicit Socket(int sockNum) noexcept;
-    explicit Socket(const std::string &address, int portNum, SocketType = SocketType::ETHERNET) noexcept;
 
     Socket(const Socket &)            = delete;
     Socket(Socket &&)                 = delete;
     Socket operator=(const Socket &s) = delete;
     Socket operator=(Socket &&)       = delete;
+
+    /**
+     * @brief Закроет сокет, если он был не закрыт по какой-то причине
+     */
     ~Socket();
 
     bool open([[maybe_unused]] OpenMode mode = OpenMode::READ_WRITE) override;
